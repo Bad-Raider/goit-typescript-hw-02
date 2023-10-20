b14027a674a5885e67778f4afef3b068c1a8059c
Stanilsav Husiev <114510611+Bad-Raider@users.noreply.github.com>
2023-10-20 14:13:16 +0300

    Update 2.ts

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -11,7 +11,7 @@ type AllType = {
   weight: number
 }
 
-function compare (top, bottom): AllType {
+function compare <T>(top: T, bottom: T): Object<T> {
   return {
     name: top.name,
     color: top.color,
