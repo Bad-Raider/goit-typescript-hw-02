9b00d508b04ab5d7060c9b3a4535239a2bcd688c
Stanilsav Husiev <114510611+Bad-Raider@users.noreply.github.com>
2023-10-26 11:31:40 +0300

    Update 3.ts

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -2,10 +2,11 @@ let some:unknown;
 some = 'Text';
 let str: string;
 
-if (some === "string") {
+if (typeof some === "string") {
     str = some;
 } else {
     console.log(`${some} is\`n "string"`);
 }
+
 
 export {};
